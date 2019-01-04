47c7ad22f0979f94d54e1b31cbbbcc9960d2498f
timvisee <timvisee@gmail.com>
2019-01-04 17:08:14 +0100

    Create field struct and display logic

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 extern crate itertools;
 
+use std::cmp::max;
 use std::fmt;
 use std::fs::read_to_string;
 use std::io::Result as IoResult;
 
 use itertools::Itertools;
+
+const EMPTY: char = '.';
 
 fn main() {
     // Load the matrices
@@ -15,6 +18,9 @@ fn main() {
 
     println!("Matrix A:\n{}", matrix_a);
     println!("Matrix B:\n{}", matrix_b);
+
+    let field = Field::from(matrix_a, matrix_b);
+    println!("Field:\n{}", field);
 }
 
 #[derive(Debug, Clone)]
@@ -23,6 +29,13 @@ pub struct Matrix {
 }
 
 impl Matrix {
+    /// Construct a new matrix.
+    pub fn new(width: usize, height: usize, default: char) -> Self {
+        Self {
+            m: vec![vec![default; width]; height],
+        }
+    }
+
     /// Load a matrix from a file at the given path.
     pub fn load(path: &str) -> IoResult<Self> {
         Ok(Self {
@@ -38,15 +51,73 @@ impl Matrix {
                 .collect::<Vec<Vec<char>>>(),
         })
     }
+
+    /// Get the width of the matrix.
+    pub fn width(&self) -> usize {
+        self.m
+            .get(0)
+            .map(|row| row.len())
+            .unwrap_or(0)
+    }
+
+    /// Get the height of the matrix.
+    pub fn height(&self) -> usize {
+        self.m.len()
+    }
+
+    /// Convert the matrix into a humanly readable string.
+    /// Characters in a row are separated by a space.
+    pub fn to_string(&self) -> String {
+        self.m
+            .iter()
+            .map(|row| row.iter().join(" "))
+            .join("\n")
+    }
 }
 
 impl fmt::Display for Matrix {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}\n", self.to_string())
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Field {
+    left: Matrix,
+    top: Matrix,
+    field: Matrix,
+}
+
+impl Field {
+    pub fn from(left: Matrix, top: Matrix) -> Self {
+        Self {
+            field: Matrix::new(top.width(), left.height(), EMPTY),
+            left,
+            top,
+        }
+    }
+}
+
+impl fmt::Display for Field {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Determine the width of the left matrix, with separating spaces
+        let left_width = max(self.left.width() as i64 * 2 - 1, 0) as usize;
+
+        // Print the top matrix first
+        write!(f, "{}\n\n",
+            self.top.to_string()
+                .lines()
+                .map(|row| format!("{}{}", vec![' '; left_width + 3].iter().join(""), row))
+                .join("\n")
+        )?;
+
+        // Print the left and field matrix
         write!(f, "{}\n",
-            self.m
-                .iter()
-                .map(|row| row.iter().join(" "))
-                .join("\n"),
+            self.left.to_string()
+                .lines()
+                .zip(self.field.to_string().lines())
+                .map(|(left, field)| format!("{}   {}", left, field))
+                .join("\n")
         )
     }
 }
